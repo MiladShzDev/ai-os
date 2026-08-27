7162aaab4afa299fe2899e06a84cfe5a20937b6d
Milad Dehbozorgi <51903274+MiladShzDev@users.noreply.github.com>
2026-08-27 14:16:25 -0700

    feat: add devices api router

--- services/api/app/api/v1/router.py
+++ services/api/app/api/v1/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .health import router as health_router
+from .devices.router import router as devices_router
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(health_router)
+router.include_router(devices_router)
