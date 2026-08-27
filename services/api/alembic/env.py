c22ea9d2fcb1f2b40b0cddf16c4951d65680fec6
Milad Dehbozorgi <51903274+MiladShzDev@users.noreply.github.com>
2026-08-27 14:10:54 -0700

    fix: load all models in alembic

--- services/api/alembic/env.py
+++ services/api/alembic/env.py
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.db import Base
-from app.models import Agent
+from app.models import Agent, Device, Application, Capability, Permission, Task
 
 target_metadata = Base.metadata
 
